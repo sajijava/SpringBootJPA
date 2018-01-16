2cc3b070c1dbfb06482788c6859f9e62f2c42223
Saji <sajijava@yahoo.com>
2018-01-16 18:45:58 -0500

    first

---
+++ src/main/java/com/example/demo/SpringBootJpaApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaApplication {
+
+	@Autowired
+	private TeamDAO teamDao;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+	@PostConstruct
+	public void init()
+	{
+		Set<Players> players = new HashSet();
+		players.add(new Players("Snoopy","Pitcher"));
+		players.add(new Players("Charle Brown","Shortstop"));
+		
+		Team team = new Team("Peanuts","California", players);
+		teamDao.save(team);
+		
+	}
+	
+}
